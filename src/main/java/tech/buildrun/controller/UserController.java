41553d75e903e712b8152532be8748cd0fd31412
Ítalo <italontab@hotmail.com>
2024-07-22 22:22:49 -0300

    feat: Adição de projeto de gerenciador de id com implementação do pattern Active Repository

---
+++ src/main/java/tech/buildrun/controller/UserController.java
@@ -0,0 +1,57 @@
+package tech.buildrun.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import tech.buildrun.entity.UserEntity;
+import tech.buildrun.service.UserService;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.UUID;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GET
+    public Response findAll( @QueryParam("page") @DefaultValue("0") Integer page,
+                                           @QueryParam("pageSize") @DefaultValue("10") Integer pageSize) {
+        var users = userService.findAll(page, pageSize);
+
+        return Response.ok(users).build();
+    }
+
+    @Transactional
+    @PUT
+    @Path("/{id}")
+    public Response updateUser(@PathParam("id") UUID userId, UserEntity userEntity){
+        return Response.ok(userService.updateUser(userId, userEntity)).build();
+    }
+
+    @Transactional
+    @POST
+    public Response createUser(UserEntity userEntity){
+        return Response.ok(userService.createUser(userEntity)).build();
+    }
+
+    @Path("/{id}")
+    @GET
+    public Response findById(@PathParam("id") UUID userId){
+        return Response.ok(userService.findById(userId)).build();
+    }
+
+    @Path("/{id}")
+    @DELETE
+    @Transactional
+    public Response deleteById(@PathParam("id") UUID userId){
+        return Response.noContent().build();
+    }
+}
---
+++ src/main/java/tech/buildrun/service/UserService.java
@@ -0,0 +1,44 @@
+package tech.buildrun.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import tech.buildrun.entity.UserEntity;
+import tech.buildrun.exception.UserNotFoundException;
+
+import java.util.List;
+import java.util.UUID;
+
+@ApplicationScoped
+public class UserService {
+
+    public UserEntity createUser(UserEntity userEntity) {
+        UserEntity.persist(userEntity);
+        return userEntity;
+    }
+
+    public List<UserEntity> findAll(Integer page, Integer pageSize) {
+        return UserEntity.findAll()
+                .page(page, pageSize)
+                .list();
+    }
+
+    public UserEntity findById(UUID userId) {
+        return (UserEntity) UserEntity.findByIdOptional(userId)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public UserEntity updateUser(UUID userId, UserEntity userEntity) {
+        var user = findById(userId);
+
+        user.username = userEntity.username;
+
+        UserEntity.persist(user);
+
+        return user;
+    }
+
+    public void deleteById(UUID userId) {
+
+        var user = findById(userId);
+        UserEntity.deleteById(user.userId);
+    }
+}
